262debc24348241f4b697f4f1b26bfccc8480a55
Senil-d <dimalkawijesundara@gmail.com>
2024-03-27 01:20:27 +0530

    add delete tour API route

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,4 +1,5 @@
 import Tour from '../models/tour.model.js';
+import { errorHandler } from '../utills/error.js';
 
 
 //create a tour...........
@@ -6,6 +7,26 @@ export const createTour = async (req, res, next) => {
   try {
     const tour = await Tour.create(req.body);
     return res.status(201).json(tour);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//delete a tour................
+export const deleteTour = async (req, res, next) => {
+  const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(errorHandler(404, 'Tour not found!'));
+  }
+
+  if (req.supplier.id !== tour.supplierRef) {
+    return next(errorHandler(401, 'You can only delete your own tours!'));
+  }
+
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json('tour has been deleted!');
   } catch (error) {
     next(error);
   }
--- routes/tour.route.js
+++ routes/tour.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createTour} from '../controllers/tour.controller.js';
+import {createTour, deleteTour} from '../controllers/tour.controller.js';
 
 
 const router = express.Router();
 
 router.post('/create', createTour);
+router.delete('/delete/:id', deleteTour);
 
 export default router;
